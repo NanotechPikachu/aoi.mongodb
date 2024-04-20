74b3119b04eeff9555c23e50b5dde8ec0d69873f
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-20 17:16:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,35 @@ module.exports = {
     setup: (Obj) => {
         const client = Obj.bot || Obj.client;
         const mongoURL = Obj.mongoURL || null;
+        const variables = Obj.variables;
 
         this.data = {
             "client": client,
             "dirname": process.cwd(),
-            "mongoURL": mongoURL
+            "mongoURL": mongoURL,
+            "variables": variables
         };
-
-      if (!mongoURL) {
-        console.error("Mongo URL not given!");
-        process.exit(1);
-      } else {
-        mongoose.connect(mongoURL, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        }).then(() => console.log("Successfully connected to Mongo DB!")).catch((err) => {
-          console.error(`Mongo DB connection failed: ${err}`));
-          process.exit(1);
+        
+        if (!variables) {
+            console.error("Variables file not found!");
+            process.exit(1);
+        } else if (typeof variables !== 'object') {
+            console.error("Variables file is not an Object!");
+            process.exit(1);
         };
-      };
+        
+        if (!mongoURL) {
+            console.error("Mongo URL not given!");
+            process.exit(1);
+        } else {
+            mongoose.connect(mongoURL, {
+              useNewUrlParser: true,
+              useUnifiedTopology: true,
+            }).then(() => console.log("Successfully connected to Mongo DB!")).catch((err) => {
+              console.error(`Mongo DB connection failed: ${err}`));
+              process.exit(1);
+            };
+          };
       
         for (const file of fs.readdirSync(path.join(__dirname, "./functions")).filter(file => file.endsWith(".js"))) {
             var funcs = require("./functions/"+file);
