e1de1a2568d057aad05c766d7ec353c87d6fbe2a
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-20 20:07:08 +0530

    Create convertType.js

---
+++ func/convertType.js
@@ -0,0 +1,26 @@
+function convertType(str) {
+  // Try to convert to number
+  if (!isNaN(str)) {
+    return Number(str);
+  };
+  
+  // Try to convert to boolean
+  if (str.toLowerCase() === 'true') {
+    return true;
+  };
+  if (str.toLowerCase() === 'false') {
+    return false;
+  };
+  
+  // Try to parse as JSON
+  try {
+    return JSON.parse(str);
+  } catch (e) {
+    // Not JSON, return as string
+  };
+  
+  // Return as string if all else fails
+  return str;
+};
+
+module.exports = { convertType };
