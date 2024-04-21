e9a22c1c353fcd4d59b19908e30aaabe5c8f6cbd
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-21 12:58:59 +0530

    Create getGlobalUserMVar.js

---
+++ functions/getGlobalUserMVar.js
@@ -0,0 +1,50 @@
+const v = require('../index.js')?.getData()?.variables;
+const GlobalUserVar = require('../schema/globalUserVar.js');
+
+module.exports = {
+  name: "$getGlobalUserMVar",
+  type: "djs",
+  code: async d => {
+    const data = d.util.aoiFunc(d);
+
+    let [ varname, userId = d.author?.id ] = data.inside.splits;
+    let res;
+
+    varname = varname?.trim();
+    userId = userId?.trim();
+
+    if (v[varname] === undefined) return d.channel.send("Variable not initailized.");
+
+    try {
+      const globalUserVariable = await GlobalUserVar.findOne({
+        userId: userId,
+        variable: varname
+      });
+
+      if (!globalUserVariable) {
+        const newAssign = await GlobalUserVar.findOneAndUpdate({
+          userId: userId,
+          variable: varname      
+        }, {
+          $set: { value: v[varname] }
+        }, {
+          upsert: true, new: true
+        });
+        newAssign.markModified();
+        await newAssign.save();
+        res = v[varname];
+      } else {
+        res = (typeof globalUserVariable.value === 'object') ? JSON.stringify(globalUserVariable.value) : globalUserVariable.value; 
+      };
+
+    } catch (err) {
+      console.error(`Error in ${data.function}. Error: ${err}`);
+      return;
+    };
+
+    data.result = res;
+    return {
+      code: d.util.setCode(data),
+    };
+  }
+};
