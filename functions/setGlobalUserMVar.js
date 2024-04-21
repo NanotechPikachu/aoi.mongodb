811749a5255bf4f5b846507c41bf0d8828f81fab
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-21 14:34:46 +0530

    Create setGlobalUserMVar.js

---
+++ functions/setGlobalUserMVar.js
@@ -0,0 +1,39 @@
+const v = require('../index.js')?.getData()?.variables;
+const GlobalUserVar = require('../schema/globalUserVar.js');
+const { convertType } = require('../func/convertType.js');
+
+module.exports = {
+  name: "$setGlobalUserMVar",
+  type: "djs",
+  code: async d => {
+    const data = d.util.aoiFunc(d);
+
+    let [ varname, value, userId = d.author?.id ] = data.inside.splits;
+    let res;
+
+    varname = varname?.trim();
+    value = value?.trim();
+    userId = userId?.trim();
+
+    // Converting the Data Type of Value depending on the input
+    value = convertType(value);
+
+    if (v[varname] === undefined) return d.channel.send("Variable not initialized.");
+
+    try {
+      const newAssign = await GlobalUserVar.findOneAndUpdate({
+        userId: userId,
+        variable: varname      
+      }, {
+        $set: { value: value }
+      }, {
+        upsert: true, new: true
+      });
+        newAssign.markModified();
+        await newAssign.save();
+    } catch (err) {
+      console.error(`Error in ${data.function}. Error: ${err}`);
+      return;
+    };
+  }
+};
