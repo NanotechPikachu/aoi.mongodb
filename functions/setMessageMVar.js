a81ed67df5204c4385c0fa81fbf09be4b8671e35
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-21 14:00:52 +0530

    Create setMessageMVar.js

---
+++ functions/setMessageMVar.js
@@ -0,0 +1,40 @@
+const v = require('../index.js')?.getData()?.variables;
+const MessageVar = require('../schema/messageVar.js');
+const { convertType } = require('../func/convertType.js');
+
+module.exports = {
+  name: "$setMessageMVar",
+  type: "djs",
+  code: async d => {
+    const data = d.util.aoiFunc(d);
+
+    let [ varname, value, messageId = d.message?.id ] = data.inside.splits;
+    let res;
+
+    varname = varname?.trim();
+    value = value?.trim();
+    messageId = messageId?.trim();
+
+    // Converting the Data Type of Value depending on the input
+    value = convertType(value);
+
+    if (v[varname] === undefined) return d.channel.send("Variable not initialized.");
+    if (!varname || varname === "" || varname === null) return "Invalid Var!";
+
+    try {
+      const newAssign = await MessageVar.findOneAndUpdate({
+        messageId: messageId,
+        variable: varname      
+      }, {
+        $set: { value: value }
+      }, {
+        upsert: true, new: true
+      });
+        newAssign.markModified();
+        await newAssign.save();
+    } catch (err) {
+      console.error(`Error in ${data.function}. Error: ${err}`);
+      return;
+    };
+  }
+};
