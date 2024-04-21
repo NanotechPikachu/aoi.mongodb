0047e3cbd65841d436c6af065269b02afdd6a33a
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-21 13:52:15 +0530

    Create setGuildMVar.js

---
+++ functions/setGuildMVar.js
@@ -0,0 +1,40 @@
+const v = require('../index.js')?.getData()?.variables;
+const GuildVar = require('../schema/guildVar.js');
+const { convertType } = require('../func/convertType.js');
+
+module.exports = {
+  name: "$setGuildMVar",
+  type: "djs",
+  code: async d => {
+    const data = d.util.aoiFunc(d);
+
+    let [ varname, value, guildId = d.guild?.id ] = data.inside.splits;
+    let res;
+
+    varname = varname?.trim();
+    value = value?.trim();
+    guildId = guildId?.trim();
+
+    // Converting the Data Type of Value depending on the input
+    value = convertType(value);
+
+    if (v[varname] === undefined) return d.channel.send("Variable not initialized.");
+    if (!varname || varname === "" || varname === null) return "Invalid Var!";
+
+    try {
+      const newAssign = await GuildVar.findOneAndUpdate({
+        guildId: guildId,
+        variable: varname      
+      }, {
+        $set: { value: value }
+      }, {
+        upsert: true, new: true
+      });
+        newAssign.markModified();
+        await newAssign.save();
+    } catch (err) {
+      console.error(`Error in ${data.function}. Error: ${err}`);
+      return;
+    };
+  }
+};
