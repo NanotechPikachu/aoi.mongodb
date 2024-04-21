4877f7334158b30471367eee7a6ff5fd49d95693
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-21 13:41:04 +0530

    Create setUserMVar.js

---
+++ functions/setUserMVar.js
@@ -0,0 +1,42 @@
+const v = require('../index.js')?.getData()?.variables;
+const UserVar = require('../schema/userVar.js');
+const { convertType } = require('../func/convertType.js');
+
+module.exports = {
+  name: "$setUserMVar",
+  type: "djs",
+  code: async d => {
+    const data = d.util.aoiFunc(d);
+
+    let [ varname, value, userId = d.author?.id, guildId = d.guild?.id ] = data.inside.splits;
+    let res;
+
+    varname = varname?.trim();
+    value = value?.trim();
+    userId = userId?.trim();
+    guildId = guildId?.trim();
+
+    // Converting the Data Type of Value depending on the input
+    value = convertType(value);
+
+    if (v[varname] === undefined) return d.channel.send("Variable not initialized.");
+    if (!varname || varname === "" || varname === null) return "Invalid Var!";
+
+    try {
+      const newAssign = await UserVar.findOneAndUpdate({
+        userId: userId,
+        guildId: guildId,
+        variable: varname      
+      }, {
+        $set: { value: value] }
+      }, {
+        upsert: true, new: true
+      });
+        newAssign.markModified();
+        await newAssign.save();
+    } catch (err) {
+      console.error(`Error in ${data.function}. Error: ${err}`);
+      return;
+    };
+  }
+};
