874f34d84007d455edbc3218d0130632fa1dbf6c
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-23 17:03:25 +0530

    Update mongoArray.js

--- functions/mongoArray.js
+++ functions/mongoArray.js
@@ -1,6 +1,15 @@
+const v = require('../index.js')?.getData()?.variables;
+const ChannelVar = require('../schema/channelVar.js');
+const GuildVar = require('../schema/guildVar.js');
+const UserVar = require('../schema/userVar.js');
+const GlobalUserVar = require('../schema/globalUserVar.js');
+const GlobalVar = require('../schema/globalVar.js');
+
+
 module.exports = {
   name: "$mongoArray",
   type: "djs",
   code: async d => {
+
   }
 };
