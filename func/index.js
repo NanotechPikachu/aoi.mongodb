e61d64c616ffa6e2c6c3a1a0b348843b8e7f5396
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-05-06 13:59:08 +0530

    Update index.js

--- func/index.js
+++ func/index.js
@@ -1,5 +1,15 @@
 const getGlobalVar = require('./getGlobalVar.js');
+const getMessageVar = require('./getMessageVar.js');
+const getGlobalUserVar = require('./getGlobalUserVar.js');
+const getUserVar = require('./getUserVar.js');
+const getGuildVar = require('./getGuildVar.js');
+const getChannelVar = require('./getChannelVar.js');
 
 module.exports = {
-  getGlobalVar
+  getGlobalVar,
+  getMessageVar,
+  getGlobalUserVar,
+  getUserVar,
+  getGuildVar,
+  getChannelVar
 };
