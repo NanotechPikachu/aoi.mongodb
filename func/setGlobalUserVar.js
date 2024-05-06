fdbdcbdbe3b460d4379f99915b2eba8b7d9f9458
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-05-06 14:38:20 +0530

    Create setGlobalUserVar.js

---
+++ func/setGlobalUserVar.js
@@ -0,0 +1,29 @@
+const v = require('../index.js')?.getData()?.variables;
+const GlobalUserVar = require('../schema/globalUserVar.js');
+const { convertType } = require('./convertType.js');
+
+async funtion setGlobalUserVar(varname, value) {
+
+  value = convertType(value);
+  if (v[varname] === undefined) return "Variable not initialized.";
+
+  try {
+      const newAssign = await GlobalUserVar.findOneAndUpdate({
+        userId: userId,
+        variable: varname      
+      }, {
+        $set: { value: value }
+      }, {
+        upsert: true, new: true
+      });
+        newAssign.markModified();
+        await newAssign.save();
+        return;
+    } catch (err) {
+      console.error(`Error in ${data.function}. Error: ${err}`);
+      return;
+    };
+
+};
+
+module.exports = setGlobalUserVar;
