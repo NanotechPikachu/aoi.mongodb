ee85a71a8e6978554026e45268a088721118a075
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-06-28 19:47:17 +0530

    Make deleteUserVar

---
+++ functions/deleteUserVar.js
@@ -0,0 +1,20 @@
+const v = require('../index.js')?.getData()?.variables;
+const UserVar = require('../schema/userVar.js');
+
+module.exports = {
+  name: "$deleteUserVar",
+  type: "djs",
+  code: async d => {
+    const data = d.util.aoiFunc(d);
+
+    let [ varname, userId = d.author?.id, guildId = d.guild?.id, returnCount = "false" ] = data.inside.splits;
+    let res;
+
+    varname = varname?.trim();
+    userId = userId?.trim();
+    guildId = guildId?.trim();
+    returnCount = returnCount?.trim()?.to lowercase();
+
+    if (v[varname] === undefined) return d.channel.send("Variable not initailized.");
+  }
+}
