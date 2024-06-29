3737ae320f5168f2f9ab19c98934d2ed9b5e058c
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-06-29 12:02:27 +0530

    Update getGuildMVar.js

--- functions/getGuildMVar.js
+++ functions/getGuildMVar.js
@@ -32,7 +32,7 @@ module.exports = {
         });
         newAssign.markModified();
         await newAssign.save();
-        res = v[varname];
+        res = (typeof v[varname] === 'object' ? JSON.stringify(v[varname]) : v[varname]);
       } else {
         res = (typeof guildVariable.value === 'object') ? JSON.stringify(guildVariable.value) : guildVariable.value; 
       };
